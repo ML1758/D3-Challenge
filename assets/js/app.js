80bad1db2c31062895a694098efc18f1ea6170ee
ML1758 <milinda.liyanage@gmail.com>
2021-08-30 18:24:54 +1000

    started optional section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,183 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 850;
+var svgHeight = 475;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// =========================================================================================
+// Initial Params
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(healthData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.8, 
+    d3.max(healthData, d => d[chosenXAxis])])
+    .range([0, width]);
+
+    return xLinearScale;
+
+};
+
+
+// function used for updating y-scale var upon click on axis label
+function yScale(healthData, chosenYAxis) {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.2, 
+      d3.max(healthData, d => d[healthcare] )])
+    .range([height, 0]);
+  return yLinearScale;
+
+};
+
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+};
+
+
+// function used for updating yAxis var upon click on axis label
+function renderAxes(newXScale, yAxis) {
+  var leftAxis = d3.axisLeft(newXScale);
+
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+
+  return yAxis;
+};
+
+// function used for updating circles group with a transition to
+// new circles when x axis is changed
+function renderXCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+};
+
+
+// function used for updating circles group with a transition to
+// new circles when y axis is changed
+function renderYCircles(circlesGroup, newXScale, chosenYAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cy", d => newYScale(d[chosenYAxis]));
+
+  return circlesGroup;
+};
+
+
+// =====================================================================================
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    //Parse Data/Cast as numbers
+    healthData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.age = +data.age;
+      data.income = +data.income;
+      data.healthcare = +data.healthcare;
+      data.obesity = +data.obesity;
+      data.smokes = +data.smokes;
+    });
+
+    // Create scale functions
+    // var xLinearScale = d3.scaleLinear()
+    //   .domain([8, d3.max(healthData, d => d.poverty)])
+    //   .range([0, width]);
+
+    // var yLinearScale = d3.scaleLinear()
+    //   .domain([2, d3.max(healthData, d => d.healthcare)])
+    //   .range([height, 0]);
+    var xLinearScale = xScale(healthData, chosenXAxis);
+    var yLinearScale = yScale(healthData, chosenYAxis);
+
+
+    // Create initial axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .classed("x-axis", true) 
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d[chosenYAxis]))
+    .attr("r", "10")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".75")
+    ;
+
+  // Create group for two x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`);
+
+
+// *****************************************************************************
+
+
+
+
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 5)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+
+
+  }).catch(function(error) {
+    console.log(error);
+});
