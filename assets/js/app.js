e84fc2ec6dff80063f59e909da76e8e3953d0520
ML1758 <milinda.liyanage@gmail.com>
2021-08-31 15:27:47 +1000

    level 1 wip text in bubble

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,163 +20,102 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// =========================================================================================
-// Initial Params
-var chosenXAxis = "poverty";
-var chosenYAxis = "healthcare";
-
-// function used for updating x-scale var upon click on axis label
-function xScale(healthData, chosenXAxis) {
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.8, 
-    d3.max(healthData, d => d[chosenXAxis])])
-    .range([0, width]);
-
-    return xLinearScale;
-
-};
-
-
-// function used for updating y-scale var upon click on axis label
-function yScale(healthData, chosenYAxis) {
-  // create scales
-  var yLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.2, 
-      d3.max(healthData, d => d[healthcare] )])
-    .range([height, 0]);
-  return yLinearScale;
-
-};
-
-
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
-
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
-
-  return xAxis;
-};
-
-
-// function used for updating yAxis var upon click on axis label
-function renderAxes(newXScale, yAxis) {
-  var leftAxis = d3.axisLeft(newXScale);
-
-  yAxis.transition()
-    .duration(1000)
-    .call(leftAxis);
-
-  return yAxis;
-};
-
-// function used for updating circles group with a transition to
-// new circles when x axis is changed
-function renderXCircles(circlesGroup, newXScale, chosenXAxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
-
-  return circlesGroup;
-};
-
-
-// function used for updating circles group with a transition to
-// new circles when y axis is changed
-function renderYCircles(circlesGroup, newXScale, chosenYAxis) {
-
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cy", d => newYScale(d[chosenYAxis]));
-
-  return circlesGroup;
-};
-
-
-// =====================================================================================
-
 // Import Data
 d3.csv("assets/data/data.csv").then(function(healthData) {
 
-    //Parse Data/Cast as numbers
+    // Parse Data/Cast as numbers
     healthData.forEach(function(data) {
       data.poverty = +data.poverty;
-      data.age = +data.age;
-      data.income = +data.income;
       data.healthcare = +data.healthcare;
-      data.obesity = +data.obesity;
-      data.smokes = +data.smokes;
     });
 
     // Create scale functions
-    // var xLinearScale = d3.scaleLinear()
-    //   .domain([8, d3.max(healthData, d => d.poverty)])
-    //   .range([0, width]);
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(healthData, d => d.poverty)])
+      .range([0, width]);
 
-    // var yLinearScale = d3.scaleLinear()
-    //   .domain([2, d3.max(healthData, d => d.healthcare)])
-    //   .range([height, 0]);
-    var xLinearScale = xScale(healthData, chosenXAxis);
-    var yLinearScale = yScale(healthData, chosenYAxis);
+    var yLinearScale = d3.scaleLinear()
+      .domain([2, d3.max(healthData, d => d.healthcare)])
+      .range([height, 0]);
 
-
-    // Create initial axis functions
-    // ==============================
+    // Create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
     // Append Axes to the chart
-    // ==============================
     chartGroup.append("g")
-      .classed("x-axis", true) 
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
 
     chartGroup.append("g")
       .call(leftAxis);
 
-    // Step 5: Create Circles
-    // ==============================
+    // Create Circles
     var circlesGroup = chartGroup.selectAll("circle")
     .data(healthData)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    .attr("cy", d => yLinearScale(d[chosenYAxis]))
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "10")
     .attr("fill", "lightblue")
     .attr("opacity", ".75")
     ;
 
-  // Create group for two x-axis labels
-  var labelsGroup = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`);
-
-
-// *****************************************************************************
-
-
-
-
-
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 5)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (height / 1.5))
       .attr("dy", "1em")
       .attr("class", "axisText")
       .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${width / 2.5}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("In Poverty (%)");
 
+    // chartGroup.append('text')
+    //    .attr("transform", `translate(${d => xLinearScale(d.poverty)}, ${d => yLinearScale(d.healthcare)})`)
+    //   .attr('text-anchor', 'middle')
+    //   .attr('alignment-baseline', 'middle')
+    //   .style('font-size', '10px')
+    //   .attr('fill-opacity', 50)
+    //   .attr('fill', 'red')
+    //   .text("xx")
+
+    // chartGroup.append("text").text(x);
+
+    console.log(healthData);
+
+    var abbr = healthData.map(({ abbr }) => abbr);
+    console.log(abbr);
+
+    var x = xLinearScale(healthData) ;
+    for (var i = 0; i < healthData.length; i++) {   
+      chartGroup.append('text')
+      .attr("transform", `translate(${healthData[i].poverty *10}, ${healthData[i].healthcare*10})`)
+     //.attr("transform", `translate(${d => xLinearScale(d.poverty)}, ${d => yLinearScale(d.healthcare)})`)
+     .attr('text-anchor', 'middle')
+     .attr('alignment-baseline', 'middle')
+     .style('font-size', '10px')
+     .attr('fill-opacity', 50)
+     .attr('fill', 'red')
+     .text(healthData[i].abbr)
+
+     //x = xLinearScale(healthData[i].poverty)
+
+     //console.log(x[i]);
+     //  console.log(healthData[i].poverty)
+    //   console.log(healthData[i].healthcare)
+    //   console.log(healthData[i].abbr);
+     
+
+    };
+
+    console.log(xLinearScale);
+    
 
   }).catch(function(error) {
     console.log(error);
