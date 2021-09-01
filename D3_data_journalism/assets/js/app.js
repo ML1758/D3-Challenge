b831a3cb485010245bcd2849f5ecab50d2c695d7
ML1758 <milinda.liyanage@gmail.com>
2021-09-01 18:51:40 +1000

    folder structure adjusted

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,95 @@
+var svgWidth = 850;
+var svgHeight = 475;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    // Parse Data/Cast as numbers
+    healthData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(healthData, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([2, d3.max(healthData, d => d.healthcare)])
+      .range([height, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Create axes labels
+    // x axis
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2.5}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");      
+    
+     // y axix
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 5)
+      .attr("x", 0 - (height / 1.5))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    // create a goroup for the circle
+    var circlesGroup = chartGroup.selectAll("g")
+      .data(healthData)
+      .enter()
+      .append("g");
+
+    // add the circles
+    circlesGroup.append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "10")
+      .attr("fill", "lightblue")
+      .attr("opacity", .75);
+
+    // add the text to the circle
+    circlesGroup.append('text')
+      .attr("dx", d => xLinearScale(d.poverty))
+      .attr("dy", d => yLinearScale(d.healthcare))      
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .style('font-size', '10px')
+      .attr('fill', 'white')
+      .text(function(d){return d.abbr});
+
+  }).catch(function(error) {
+    console.log(error);
+});
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # D3-Challenge
-D3 Assignment - ML
+D3 Assignment - Milinda 'ML' Liyanage
+
+## Summary
+
+* This assignment is to use D3java script and plotly to read data and display interactive charts.
+* The application allows a user to select an ID which display the relevant master data and the corresponding top 10 values in a horizontal bar chart and the details in a bubble chart.
+* As an optional section, a gauge chart is displayed with a single attribute.
+
+### The following tasks were done for the main requirement: 
+
+* The data was in json format and it was read through d3.
+* A function was created to append the IDs to the dropdown list.
+* The HTML code was changed to run the main function (updatePlotly), when the list value is changed.
+* As the application starts, all the information in the page is prepopulated with the first ID’s data.  
+* The main function read the selected ID and runs the metadata pane and the three charts. The ID is passed when calling each of the functions.
+* The ID was converted to a an init before passing to two of the functions. 
+* The metadata function read the data then filter by the entered ID and then display.
+* The bar chart function read the data then filter by the entered ID and then slice to get the first 10 value set.
+* The bar chart is horizontal and have values displayed from large to small, i.e., reversed.
+* The bubble chart function read the data then filter by the entered ID and then display.
+
+### Optional requirement:
+
+* A gauge chart function was created to read the data filter by the entered ID.
+* The gauge display a single attribute.
